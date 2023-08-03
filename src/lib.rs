2ee52b06a77643d2d4f2588883df17e556a3a415
Semen Yesylevskyy <yesint4@yahoo.com>
2023-08-03 15:26:03 +0200

    Dev

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
-use std::ffi::{CString,CStr};
-use std::os::raw::{c_char,c_void,c_int};
-use std::{ptr, error};
-use molfile_bindings::{molfile_plugin_t, molfile_atom_t,
-    pdb_get_plugin_ptr, xyz_get_plugin_ptr, dcd_get_plugin_ptr,
-    MOLFILE_SUCCESS, MOLFILE_EOF, molfile_timestep_t};
-use std::io::{Error,ErrorKind};
-use std::path::Path;
+use molfile_bindings::{
+    dcd_get_plugin_ptr, molfile_atom_t, molfile_plugin_t, molfile_timestep_t, pdb_get_plugin_ptr,
+    xyz_get_plugin_ptr, MOLFILE_EOF, MOLFILE_SUCCESS,
+};
+use std::ffi::{c_void, CStr, CString};
+use std::{error, ptr};
+
+use ascii::{AsciiChar, AsciiString};
 use std::default::Default;
-use ascii::{AsciiString,AsciiChar};
+use std::path::Path;
+
+use anyhow::{anyhow, bail, Result};
 
 #[allow(dead_code)]
 #[allow(non_upper_case_globals)]
@@ -15,8 +17,8 @@ use ascii::{AsciiString,AsciiChar};
 #[allow(non_snake_case)]
 mod molfile_bindings;
 
-
-#[derive(Debug,Default,Clone)]
+#[allow(dead_code)]
+#[derive(Debug, Default, Clone)]
 pub struct Atom {
     name: AsciiString,
     resname: AsciiString,
@@ -33,13 +35,13 @@ impl Atom {
     }
 }
 
-
-#[derive(Debug,Default,Clone)]
+#[allow(dead_code)]
+#[derive(Debug, Default, Clone)]
 pub struct Structure {
     pub atoms: Vec<Atom>,
-    bonds: Vec<[usize;2]>,
-    angles: Vec<[usize;3]>,
-    dihedrals: Vec<[usize;4]>,
+    pub bonds: Vec<[usize; 2]>,
+    pub angles: Vec<[usize; 3]>,
+    pub dihedrals: Vec<[usize; 4]>,
 }
 
 impl Structure {
@@ -48,10 +50,9 @@ impl Structure {
     }
 }
 
-
-#[derive(Debug,Default)]
+#[derive(Debug, Default)]
 pub struct State {
-    pub coords: Vec<[f32;3]>,
+    pub coords: Vec<[f32; 3]>,
 }
 
 impl State {
@@ -60,50 +61,53 @@ impl State {
     }
 }
 
-
 // Traits for different file types
-trait MolfileStructure {
-    fn read_structure(&self) -> Result<Structure,std::io::Error>;
-    fn write_structure(&self, data: &Structure) -> Result<(),std::io::Error>;
+pub trait MolfileStructure {
+    fn read_structure(&mut self) -> Result<Structure>;
+    fn write_structure(&self, data: &Structure) -> Result<()>;
 }
 
-trait MolfileSingleFrame {
-    fn read_state(&self) -> Result<State,std::io::Error>;
-    fn write_state(&self, data: &State) -> Result<(),std::io::Error>;
+pub trait MolfileSingleFrame {
+    fn read_state(&mut self) -> Result<Option<State>>;
+    fn write_state(&self, data: &State) -> Result<()>;
 }
 
+enum OpenMode {
+    Read,
+    Write,
+    None,
+}
 
-enum OpenMode {Read, Write, None}
-
-pub struct MolFileHandler<'a> {
+pub struct VmdMolFileHandler<'a> {
     // File
     file_name: String,
     // Plugin pointer
     plugin: &'a molfile_plugin_t,
-    // File handle for C 
+    // File handle for C
     file_handle: *mut c_void,
     mode: OpenMode,
     natoms: usize,
 }
 
 // Helper convertion function from C-wrapped fixed-size string to AsciiString
-fn c_buf_to_ascii_str(buf: &[::std::os::raw::c_char]) -> Result<AsciiString,Box<dyn error::Error>> {
+fn c_buf_to_ascii_str(buf: &[::std::os::raw::c_char]) -> AsciiString {
     let cstr = unsafe { CStr::from_ptr(buf.as_ptr()).to_bytes() };
     let s = unsafe { AsciiString::from_ascii_unchecked(cstr) };
-    Ok(s)
+    s
 }
 
 #[doc = "Universal handler of different file formats"]
-impl MolFileHandler<'_> {
-    
+impl VmdMolFileHandler<'_> {
     pub fn new(fname: &str) -> Self {
         // Get extention
         let ext = Path::new(fname)
-            .extension().expect("File with extension expected!")
-            .to_str().unwrap();
-        
+            .extension()
+            .expect("File with extension expected!")
+            .to_str()
+            .unwrap();
+
         // Get plugin pointer
-        // C funtion registers plugin on first call 
+        // C funtion registers plugin on first call
         // and returns stored pointer on later invocations
         let plugin = unsafe {
             match ext {
@@ -111,25 +115,27 @@ impl MolFileHandler<'_> {
                 "xyz" => xyz_get_plugin_ptr(),
                 "dcd" => dcd_get_plugin_ptr(),
                 &_ => panic!("Unrecognized extention {ext}!"),
-            }.as_ref().unwrap()
+            }
+            .as_ref()
+            .unwrap()
         };
 
         // We can't open file here because for writing we need to know natoms,
         // which is only visible in actuall call to write.
         // For reading we can open, but for consistency we'll defer it as well.
-        
+
         // Return file handler
-        MolFileHandler {
+        VmdMolFileHandler {
             file_name: fname.to_owned(),
             plugin,
             file_handle: ptr::null_mut(),
             mode: OpenMode::None, // Not opened yet
-            natoms: 0
+            natoms: 0,
         }
     }
-    
+
     // Returns number of atoms
-    fn open_read(&mut self) -> Result<(),Box<dyn error::Error>> {
+    fn open_read(&mut self) -> Result<()> {
         // Open file and get file pointer
         // Prepare c-strings for file opening
         let f_type = CString::new("").unwrap(); // Not used
@@ -143,15 +149,15 @@ impl MolFileHandler<'_> {
         self.natoms = n as usize;
 
         if self.file_handle == ptr::null_mut() {
-            Err(format!("Can't open file {} for reading!",self.file_name))?;
+            bail!("Can't open file {} for reading!", self.file_name);
         }
 
         self.mode = OpenMode::Read;
-        
+
         Ok(())
     }
 
-    fn open_write(&mut self, natoms: i32) {
+    fn open_write(&mut self, natoms: i32) -> Result<()> {
         // Open file and get file pointer
         // Prepare c-strings for file opening
         let f_type = CString::new("").unwrap(); // Not used
@@ -163,95 +169,120 @@ impl MolFileHandler<'_> {
         };
 
         if self.file_handle == ptr::null_mut() {
-            panic!("Can't open file {} for writing!",self.file_name);
+            bail!("Can't open file {} for writing!", self.file_name);
         }
 
         self.mode = OpenMode::Write;
+
+        Ok(())
     }
-    
-    pub fn read_structure(&mut self) -> Result<Structure,Box<dyn error::Error>> {
+}
+
+impl MolfileStructure for VmdMolFileHandler<'_> {
+    fn read_structure(&mut self) -> Result<Structure> {
         // Open file for reading
         match self.mode {
-            OpenMode::None => self.open_read(), // Open file
-            OpenMode::Read => Ok(()),
-            OpenMode::Write => Err("File is already opened for writing")?,
-        };
+            OpenMode::None => self.open_read()?, // Open file
+            OpenMode::Read => (),
+            OpenMode::Write => bail!("File is already opened for writing"),
+        }
 
         let mut optflags: i32 = 0;
         let el: molfile_atom_t = Default::default();
         let mut atoms = vec![el; self.natoms];
 
-        let ret = unsafe{
-            self.plugin.read_structure.unwrap()
-            (self.file_handle, &mut optflags, atoms.as_mut_ptr()) 
+        let ret = unsafe {
+            self.plugin.read_structure.unwrap()(self.file_handle, &mut optflags, atoms.as_mut_ptr())
         };
 
         if ret != MOLFILE_SUCCESS {
-            Err("Plugin error reading structure!")?;
+            bail!("Plugin error reading structure!");
         }
 
         // Convert to Structure
         let mut structure: Structure = Default::default();
         structure.atoms.reserve(self.natoms);
         for ref at in atoms {
-            let atom = Atom {
-                name: c_buf_to_ascii_str(&at.name)?,
+            structure.atoms.push(Atom {
+                name: c_buf_to_ascii_str(&at.name),
                 resid: at.resid as isize,
-                resname: c_buf_to_ascii_str(&at.resname)?,
-                chain: c_buf_to_ascii_str(&at.chain)?.first().unwrap(),
+                resname: c_buf_to_ascii_str(&at.resname),
+                chain: c_buf_to_ascii_str(&at.chain).first().unwrap(),
                 mass: at.mass,
                 charge: at.charge,
-            };
-            structure.atoms.push(atom);
+            });
         }
 
         Ok(structure)
     }
-    
-    pub fn read_state(&mut self) -> Result<State,Box<dyn error::Error>> {
+
+    fn write_structure(&self, data: &Structure) -> Result<()> {
+        Ok(())
+    }
+}
+
+impl MolfileSingleFrame for VmdMolFileHandler<'_> {
+    fn read_state(&mut self) -> Result<Option<State>> {
         // Open file for reading
         match self.mode {
-            OpenMode::None => Err("Can't read state before structure")?,
+            OpenMode::None => bail!("Can't read state before structure"),
             OpenMode::Read => (),
-            OpenMode::Write => Err("File is already opened for writing")?,
+            OpenMode::Write => bail!("File is already opened for writing"),
         }
 
         let mut st: State = Default::default();
-        st.coords = vec![[0.0,0.0,0.0]; self.natoms as usize]; 
+                
+        //st.coords = vec![[0.0, 0.0, 0.0]; self.natoms as usize];
+        
+        // Allocate storage for coordinates, but don't initialize them
+        st.coords = Vec::with_capacity(self.natoms as usize);
 
         //let mut coord = vec![0.0; 3*self.natoms as usize];
-        let mut ts = molfile_timestep_t{
-            coords: st.coords.as_mut_ptr().cast::<f32>(),
+        let mut ts = molfile_timestep_t {
+            coords: st.coords.as_mut_ptr().cast::<f32>(), // Raw ptr to allocated storage
             velocities: ptr::null_mut(),
-            A: 0.0, B: 0.0, C: 0.0, alpha: 0.0, beta: 0.0, gamma: 0.0,
+            A: 0.0,
+            B: 0.0,
+            C: 0.0,
+            alpha: 0.0,
+            beta: 0.0,
+            gamma: 0.0,
             physical_time: 0.0,
         };
 
-        let ret = unsafe{
-            self.plugin.read_next_timestep.unwrap()
-            (self.file_handle,self.natoms.try_into().unwrap(),&mut ts) 
+        let ret = unsafe {
+            self.plugin.read_next_timestep.unwrap()(
+                self.file_handle,
+                self.natoms.try_into().unwrap(),
+                &mut ts,
+            )
         };
 
+        // C function populated the coordinates, set the vector size for Rust
+        unsafe {st.coords.set_len(self.natoms as usize)}
+
         match ret {
-            MOLFILE_SUCCESS => (), // pass trough
-            MOLFILE_EOF => (), // return without reading
-            _ => Err("Error reading PDB timestep!")?,
+            MOLFILE_SUCCESS => Ok(Some(st)),
+            MOLFILE_EOF => Ok(None),
+            _ => bail!("Error reading PDB timestep!"),
         }
-
-        // Convert to pteros data structures
-         
-
-        Ok(st)
     }
 
+    fn write_state(&self, data: &State) -> Result<()> {
+        Ok(())
+    }
 }
 
-impl Drop for MolFileHandler<'_> {
+impl Drop for VmdMolFileHandler<'_> {
     fn drop(&mut self) {
         if self.file_handle != ptr::null_mut() {
             match self.mode {
-                OpenMode::Read => unsafe{ self.plugin.close_file_read.unwrap()(self.file_handle) },
-                OpenMode::Write => unsafe{ self.plugin.close_file_write.unwrap()(self.file_handle) },
+                OpenMode::Read => unsafe {
+                    self.plugin.close_file_read.unwrap()(self.file_handle)
+                },
+                OpenMode::Write => unsafe {
+                    self.plugin.close_file_write.unwrap()(self.file_handle)
+                },
                 OpenMode::None => panic!("This should never happen!"),
             };
         }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
-use pteros::MolFileHandler;
+use pteros::VmdMolFileHandler;
+use pteros::{MolfileSingleFrame, MolfileStructure};
 
 fn main() {
-    let mut h = MolFileHandler::new("colored.pdb");
+    let mut h = VmdMolFileHandler::new("colored.pdb");
     let f = h.read_structure().unwrap();
-    //for a in f.atoms {
-    //    println!("{:?}",a);
-    //}
-    let st = h.read_state().unwrap(); 
-    for c in st.coords {
-        println!("{:?}",c);
+    for _a in f.atoms {
+        //println!("{:?}",a);
+    }
+
+    for _i in 0..2 {
+        let st = h.read_state().unwrap();
+        if let Some(s) = st {
+            println!("{}", s.coords.len());
+            for c in s.coords {
+                println!("{:?}",c);
+            }
+        } else {
+            println!("EOF reached");
+        }
     }
 }
