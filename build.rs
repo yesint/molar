35888bab9e240b127b8280da018ff3b8d7a9b213
Semen Yesykevskyy <yesint4@yahoo.com>
2023-08-31 13:22:44 +0200

    dev

--- build.rs
+++ build.rs
@@ -3,10 +3,16 @@ use cmake::Config;
 fn main() {
     // Run CMake for dependencies
     for dep_name in ["molfile","xdrfile"] {
-        let dst = Config::new(format!("src/{}",dep_name))
-            .define("CMAKE_BUILD_TYPE","Release")
+        let dst = Config::new(format!("src/third_party/{dep_name}"))
+            //.define("CMAKE_BUILD_TYPE","Release")
+            .profile("Release")
             .build();
         println!("cargo:rustc-link-search=native={}", dst.display());
         println!("cargo:rustc-link-lib=static={}", dep_name);
     }
+
+    let dst = Config::new("src/third_party/gromacs").build();
+    println!("cargo:rustc-link-search=native={}", dst.display());
+    println!("cargo:rustc-link-lib=static={}", "gromacs");
+    println!("cargo:rustc-link-lib=static={}", "muparser");
 }
