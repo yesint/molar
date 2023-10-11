a825b7827a8941ec9aa44d7092dd29dda58bce14
yesint4 <yesint4@yahoo.com>
2023-10-11 20:10:14 +0200

    dev

--- src/core/selection.rs
+++ src/core/selection.rs
@@ -9,162 +9,135 @@ use super::{
 use anyhow::{bail, Result};
 use nalgebra::Point3;
 
-#[derive(Debug)]
-struct Selection<'a, I: IndexIterator> {
-    ast: Option<SelectionAst>,
-    index_iter: I,
-    phantom: PhantomData<&'a I>,
+// There are following distinct states of the Selection:
+//  Selection::from_iter(0..10).iter(structure,state)
+//  Selection::from_expr("name_CA").iter(structure,state)
+
+struct Particle<'a> {
+    id: usize,
+    atom: &'a Atom,
+    pos: &'a Pos,
 }
 
-impl<'a, I: IndexIterator> Selection<'a, I> {
-    /// Creates selection from any iterator over usize
-    fn from_iter(iter: I) -> Self {
-        Self {
-            index_iter: iter,
-            ast: None,
-            phantom: PhantomData,
-        }
-    }
-
-    fn iter_pos(&self, state: &'a State) -> impl PosIterator<'a> + '_ {
-        self.index_iter.clone().map(|i| &state.coords[i])
-    }
-
-    fn iter_pos_mut(&self, state: &'a mut State) -> impl PosIteratorMut<'a> + '_ {
-        SubscriptedMutIter {
-            iter: state.coords.iter_mut(),
-            index_iter: self.index_iter.clone(),
-            cur: 0,
-        }
-    }
-
-    fn iter_index_pos(&self, state: &'a State) -> impl IdPosIterator<'a> + '_ {
-        self.index_iter.clone().map(|i| (i, &state.coords[i]))
-    }
-
-    fn iter_index_pos_mut(&self, state: &'a mut State) -> impl IdPosIteratorMut<'a> + '_ {
-        let it = SubscriptedMutIter {
-            iter: state.coords.iter_mut(),
-            index_iter: self.index_iter.clone(),
-            cur: 0,
-        };
-        core::iter::zip(self.index_iter.clone(), it)
-    }
-
-    fn iter_atoms(&self, structure: &'a Structure) -> impl AtomIterator<'a> + '_ {
-        self.index_iter.clone().map(|i| &structure.atoms[i])
-    }
-
-    fn iter_atoms_mut(&self, structure: &'a mut Structure) -> impl AtomIteratorMut + '_ {
-        SubscriptedMutIter {
-            iter: structure.atoms.iter_mut(),
-            index_iter: self.index_iter.clone(),
-            cur: 0,
-        }
-    }
-
-    fn iter_index_atoms(&self, structure: &'a Structure) -> impl IdAtomIterator<'a> + '_ {
-        self.index_iter.clone().map(|i| (i, &structure.atoms[i]))
-    }
-
-    fn iter_index_atoms_mut(&self, structure: &'a mut Structure) -> impl IdAtomIteratorMut + '_ {
-        let it = SubscriptedMutIter {
-            iter: structure.atoms.iter_mut(),
-            index_iter: self.index_iter.clone(),
-            cur: 0,
-        };
-        core::iter::zip(self.index_iter.clone(), it)
-    }
-
-    fn iter_index_atoms_pos(
-        &self,
-        structure: &'a Structure,
-        state: &'a State,
-    ) -> impl IdAtomPosIterator<'a> + '_ {
-        self.index_iter
-            .clone()
-            .map(|i| (i, &structure.atoms[i], &state.coords[i]))
-    }
-
-    fn iter_index_atoms_pos_mut(
-        &self,
-        structure: &'a mut Structure,
-        state: &'a mut State,
-    ) -> impl IdAtomPosIteratorMut + '_ {
-        let at = SubscriptedMutIter {
-            iter: structure.atoms.iter_mut(),
-            index_iter: self.index_iter.clone(),
-            cur: 0,
-        };
-        let pos = SubscriptedMutIter {
-            iter: state.coords.iter_mut(),
-            index_iter: self.index_iter.clone(),
-            cur: 0,
-        };
-        itertools::izip!(self.index_iter.clone(), at, pos)
-    }
+struct ParticleMut<'a> {
+    id: usize,
+    atom: &'a mut Atom,
+    pos: &'a mut Pos,
 }
 
-impl Selection<'_, <Vec<usize> as IntoIterator>::IntoIter> {
-    fn from_sel_str(sel_str: &str, structure: &Structure, state: &State) -> Result<Self> {
+
+pub trait ParticleIterator<'a>: ExactSizeIterator<Item = Particle<'a>> {}
+impl<'a, T> ParticleIterator<'a> for T where T: ExactSizeIterator<Item = Particle<'a>> {}
+
+pub trait ParticleMutIterator<'a>: ExactSizeIterator<Item = ParticleMut<'a>> {}
+impl<'a, T> ParticleMutIterator<'a> for T where T: ExactSizeIterator<Item = ParticleMut<'a>> {}
+
+
+struct Selection {}
+struct SelectionWithAst(SelectionAst);
+struct SelectionWithIter<I: IndexIterator>(I);
+
+impl Selection {
+    fn from_expr(sel_str: &str) -> Result<SelectionWithAst> {
         let ast = generate_ast(sel_str)?;
-        let it = apply_ast_whole(&ast, structure, state)?;
-        Ok(Self {
-            index_iter: it.into_iter(),
-            ast: Some(ast),
-            phantom: PhantomData,
+        Ok(SelectionWithAst(ast))
+    }
+
+    fn from_iter<I: IndexIterator>(iter: I) -> SelectionWithIter<I> {
+        SelectionWithIter(iter)
+    }
+}
+
+impl SelectionWithAst {
+    fn apply<'a>(&self,structure: &'a Structure, state: &'a State) -> Result<impl ParticleIterator<'a>> {
+        let vec = apply_ast_whole(&self.0, structure, state)?;
+        Ok(vec.into_iter().map(|i| Particle{
+            id: i,
+            atom: &structure.atoms[i],
+            pos: &state.coords[i]
+        }))
+    }
+
+    fn apply_mut<'a>(&self,structure: &'a mut Structure, state: &'a mut State) -> Result<impl ParticleMutIterator<'a>> {
+        let vec = apply_ast_whole(&self.0, structure, state)?;
+        Ok(ParticleMutIteratorAdaptor {
+            atom_iter: structure.atoms.iter_mut(),
+            pos_iter: state.coords.iter_mut(),
+            index_iter: vec.into_iter(),
+            cur: 0,
         })
     }
+}
 
-    fn apply(&mut self, structure: &Structure, state: &State) -> Result<()> {
-        if let Some(ast) = self.ast.as_ref() {
-            self.index_iter = apply_ast_whole(&ast, structure, state)?.into_iter()
-        }
-        Ok(())
+impl<I: IndexIterator> SelectionWithIter<I> {
+    fn apply<'a>(&self,structure: &'a Structure, state: &'a State) -> Result<impl ParticleIterator<'a>> {
+        Ok(self.0.clone().map(|i| Particle{
+            id: i,
+            atom: &structure.atoms[i],
+            pos: &state.coords[i]
+        }))
+    }
+
+    fn apply_mut<'a>(&self,structure: &'a mut Structure, state: &'a mut State) -> Result<impl ParticleMutIterator<'a>> {
+        Ok(ParticleMutIteratorAdaptor {
+            atom_iter: structure.atoms.iter_mut(),
+            pos_iter: state.coords.iter_mut(),
+            index_iter: self.0.clone(),
+            cur: 0,
+        })
     }
 }
+
+//---------------------------------------
+
 
 // Helper struct for creating subscripted mutable iterator
 // from container iterator and index iterator
-// IMPORTANT! Only works for **sorted** container iterator!
-struct SubscriptedMutIter<'a, CI, T, II>
+// IMPORTANT! Only works for **sorted** indexes!
+struct ParticleMutIteratorAdaptor<'a, AtomI, PosI, IndexI>
 where
-    T: 'a,                          // Element type
-    CI: Iterator<Item = &'a mut T>, // Container iterator
-    II: IndexIterator,              // Index iterator
+    AtomI: Iterator<Item = &'a mut Atom>, // iterator over atoms
+    PosI: Iterator<Item = &'a mut Pos>, // iterator over positions
+    IndexI: IndexIterator,              // Index iterator
 {
-    iter: CI,
-    index_iter: II,
+    atom_iter: AtomI,
+    pos_iter: PosI,
+    index_iter: IndexI,
     cur: usize,
 }
 
-impl<'a, CI, T, II> Iterator for SubscriptedMutIter<'a, CI, T, II>
+impl<'a, AtomI, PosI, IndexI> Iterator for ParticleMutIteratorAdaptor<'a, AtomI, PosI, IndexI>
 where
-    T: 'a,
-    CI: Iterator<Item = &'a mut T>,
-    II: IndexIterator,
+    AtomI: Iterator<Item = &'a mut Atom>, // iterator over atoms
+    PosI: Iterator<Item = &'a mut Pos>, // iterator over positions
+    IndexI: IndexIterator,              // Index iterator
 {
-    type Item = &'a mut T;
+    type Item = ParticleMut<'a>;
 
     fn next(&mut self) -> Option<Self::Item> {
         match self.index_iter.next() {
             Some(i) => {
                 // Advance pos_iter by offset and yield
-                let el = self.iter.nth(i - self.cur);
+                let at = self.atom_iter.nth(i - self.cur)?;
+                let pos = self.pos_iter.nth(i - self.cur)?;
                 // Advance current position
                 self.cur = i + 1;
-                el
+                Some(ParticleMut{
+                    atom: at,
+                    pos: pos,
+                    id: i,
+                })
             }
             None => None,
         }
     }
 }
 
-impl<'a, CI, T, II> ExactSizeIterator for SubscriptedMutIter<'a, CI, T, II>
+impl<'a, AtomI, PosI, IndexI> ExactSizeIterator for ParticleMutIteratorAdaptor<'a, AtomI, PosI, IndexI>
 where
-    T: 'a,
-    CI: Iterator<Item = &'a mut T>,
-    II: IndexIterator,
+    AtomI: Iterator<Item = &'a mut Atom>, // iterator over atoms
+    PosI: Iterator<Item = &'a mut Pos>, // iterator over positions
+    IndexI: IndexIterator,              // Index iterator
 {
     fn len(&self) -> usize {
         self.index_iter.len()
