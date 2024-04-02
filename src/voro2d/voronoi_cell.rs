fc3ed74a9b9f2d30257946b556de059af402fdca
yesint <yesint4@yahoo.com>
2024-04-02 12:39:11 +0200

    working 2d voronoi

--- src/voro2d/voronoi_cell.rs
+++ src/voro2d/voronoi_cell.rs
@@ -4,22 +4,26 @@ use std::io::Write;
 pub type Vector2 = nalgebra::Vector2<f32>;
 const TOL: f32 = 1e-10;
 
-struct Vertex {
+#[derive(Debug)]
+pub struct Vertex {
     pos: Vector2,
-    ccw: usize,
-    cw: usize,
+    // Index of neighbour in ccw direction
+    ccw_neib: usize, 
+    // ID of point that created the ccw edge
+    // Negative numbers are reserved for the walls
+    edge_id: i32, 
 }
 
 struct CuttingLine {
     pos: Vector2,
-    r: f32,
+    r2: f32,
 }
 
 impl CuttingLine {
     fn new(point: &Vector2) -> Self {
         let pos = 0.5*point;
-        let r = pos.norm();
-        Self {pos, r}
+        let r2 = pos.norm_squared();
+        Self {pos, r2}
     }
 }
 
@@ -30,7 +34,7 @@ pub struct VoronoiCell {
 }
 
 #[derive(Default,Debug)]
-struct CutEdge {
+struct EdgeCut {
     in_ind: usize,
     in_dist: f32,
     out_ind: usize,
@@ -40,7 +44,7 @@ struct CutEdge {
 impl VoronoiCell {
     pub fn new(xmin: f32, xmax: f32, ymin: f32, ymax: f32) -> Self {
         let mut ret = Self {
-            vert: Default::default(),
+            vert: Vec::with_capacity(8),
             init_vert: 0,
         };
 
@@ -50,35 +54,40 @@ impl VoronoiCell {
         //  |                     |
         //(0)xmin,ymin-----------(1)xmax,ymin
         
-        ret.vert.push(Vertex{pos: Vector2::new(xmin, ymin), ccw: 1, cw: 3});
-        ret.vert.push(Vertex{pos: Vector2::new(xmax, ymin), ccw: 2, cw: 0});
-        ret.vert.push(Vertex{pos: Vector2::new(xmax, ymax), ccw: 3, cw: 1});
-        ret.vert.push(Vertex{pos: Vector2::new(xmin, ymax), ccw: 0, cw: 2});
+        ret.vert.push(Vertex{pos: Vector2::new(xmin, ymin), ccw_neib: 1, edge_id: -1});
+        ret.vert.push(Vertex{pos: Vector2::new(xmax, ymin), ccw_neib: 2, edge_id: -2});
+        ret.vert.push(Vertex{pos: Vector2::new(xmax, ymax), ccw_neib: 3, edge_id: -3});
+        ret.vert.push(Vertex{pos: Vector2::new(xmin, ymax), ccw_neib: 0, edge_id: -4});
 
         ret
     }
 
     #[inline(always)]
     fn dist(&self, i: usize, line: &CuttingLine) -> f32 {
-        (line.pos.dot(&self.vert[i].pos)-line.r*line.r) / line.r
+        line.pos.dot(&self.pos(i))-line.r2
     }
 
     #[inline(always)]
     fn next(&self, i: usize) -> usize {
-        self.vert[i].ccw
+        unsafe{ self.vert.get_unchecked(i).ccw_neib }
     }
 
     #[inline(always)]
     fn pos(&self, i: usize) -> &Vector2 {
-        &self.vert[i].pos
+        unsafe{ &self.vert.get_unchecked(i).pos }
     }
 
     #[inline(always)]
-    fn vertex(&mut self, i: usize) -> &mut Vertex {
-        &mut self.vert[i]
+    fn vertex(&self, i: usize) -> &Vertex {
+        unsafe{ self.vert.get_unchecked(i) }
     }
 
-    pub fn add_point(&mut self,point: &Vector2) -> bool {
+    #[inline(always)]
+    fn vertex_mut(&mut self, i: usize) -> &mut Vertex {
+        unsafe{ self.vert.get_unchecked_mut(i) }
+    }
+
+    pub fn add_point(&mut self,point: &Vector2, id: usize) -> bool {
         let line = CuttingLine::new(point);
         let cut1;
         let cut2;
@@ -92,8 +101,6 @@ impl VoronoiCell {
             cur_i = self.next(cur_i);
             cur_d = self.dist(cur_i, &line)
         }
-
-        //println!("First inside point: {cur_i}");
 
         // Now we are at first inside point. Update init point
         self.init_vert = cur_i;
@@ -109,7 +116,7 @@ impl VoronoiCell {
             if next_d >= TOL {
                 // next_i is the first outside point
                 // cur_i is the last inside point
-                cut1 = CutEdge {
+                cut1 = EdgeCut {
                     in_ind: cur_i,
                     in_dist: cur_d,
                     out_ind: next_i,
@@ -124,8 +131,6 @@ impl VoronoiCell {
             cur_d = next_d;
         }
 
-       // println!("First outside point: {cur_i}");
-
         // Now cur_i is the first outside point
         // Search again for the next inside point
         loop {
@@ -134,82 +139,124 @@ impl VoronoiCell {
             if next_d < TOL {
                 // next_i is the first inside point
                 // cur_i is the last outside point
-                cut2 = CutEdge {
+                cut2 = EdgeCut {
                     out_ind: cur_i,
                     out_dist: cur_d,
                     in_ind: next_i,
                     in_dist: next_d
                 };
-                //cur_i = next_i;
-                //cur_d = next_d;
                 break;
             }
             cur_i = next_i;
             cur_d = next_d;
         }
         
-        //println!("Second inside point: {cur_i}");
-        //println!("Cut 1: {cut1:?}");
-        //println!("Cut 1: {cut2:?}");
-
         // At this point both cuts are found.
-        
-        // Cut #1:
-        // For first cut we can always reuse the outer point
-        // But we need to defer modifying the point coordinates
-        // Until second cut is done because they may share the same outer point
-        let cut1_point = self.vertex_pos_from_cut(&cut1);
-        // Connection to cut2 will be set later
         
         // Cut #2:
         let p = self.vertex_pos_from_cut(&cut2);
-        // Check if we can reuse the outer point (should not be the same point as for cut1)
+        // Check if we can reuse the outer point 
+        // (should not be the same point as for cut1)
         if cut1.out_ind != cut2.out_ind {
-            let v = self.vertex(cut2.out_ind);
-            v.pos = p;
-            v.cw = cut1.out_ind; // Link to vertex from first cut
-            // Set missing ccw connection cut1->cut2
-            self.vertex(cut1.out_ind).ccw = cut2.out_ind;
+            self.vertex_mut(cut2.out_ind).pos = p;
+            // Set ccw connection cut1->cut2
+            // For first cut we always reuse its outer point
+            self.vertex_mut(cut1.out_ind).ccw_neib = cut2.out_ind;
         } else {
             // There is only one outer point, so we need to add new vertex
+            let old_id = self.vertex(cut2.out_ind).edge_id;
             self.vert.push(
                 Vertex {
                     pos: p,
-                    cw: cut1.out_ind, // Link to vertes from first cut
-                    ccw: cut2.in_ind, // Reset connection to next inner point
+                    ccw_neib: cut2.in_ind, // Reset connection to next inner point
+                    edge_id: old_id // Reset ID
                 }
             );
-            // Set missing ccw connection cut1->cut2
-            self.vertex(cut1.out_ind).ccw = self.vert.len()-1; // New added point
+            // Set ccw connection cut1->cut2
+            // For first cut we always reuse its outer point
+            self.vertex_mut(cut1.out_ind).ccw_neib = self.vert.len()-1; // New added point
         }
 
-        // Defered update of outer point in cut1
-        self.vertex(cut1.out_ind).pos = cut1_point;
+        // Cut #1:
+        // For first we always reuse the outer point
+        // Second cut is already done so no problem if 
+        // they share the same outer point
+        self.vertex_mut(cut1.out_ind).pos = self.vertex_pos_from_cut(&cut1);
+        self.vertex_mut(cut1.out_ind).edge_id = id as i32;
         
         true
     }
 
     #[inline(always)]
-    fn vertex_pos_from_cut(&self, cut: &CutEdge) -> Vector2 {
+    fn vertex_pos_from_cut(&self, cut: &EdgeCut) -> Vector2 {
         let frac = cut.out_dist / (cut.in_dist.abs()+cut.out_dist);
-        // New point
-        //self.pos(cut.out_ind)+frac*(self.pos(cut.in_ind)-self.pos(cut.out_ind))
         (1.0-frac)*self.pos(cut.out_ind)+frac*self.pos(cut.in_ind)
+    }
+
+    pub fn iter_vertex(&self) -> VoronoiCellVertexIter {
+        VoronoiCellVertexIter{
+            cell: self,
+            cur: self.init_vert,
+            stop: false
+        }
     }
 
     pub fn write_to_file(&self, fname: &str) {
         let mut s = String::new();
-        let mut i = self.init_vert;
-        loop {
-            s.push_str(&format!("{} {}\n",self.vert[i].pos[0],self.vert[i].pos[1]));
-            i = self.vert[i].ccw;
-            if i == self.init_vert {break}
-        }
+        self.for_each_vert(
+            |v| s.push_str(&format!("{} {}\n",v.pos[0],v.pos[1]))
+        );
         let mut out = File::create(fname).unwrap();
         write!(out,"{}",s).unwrap();
     }
 
+    pub fn area(&self) -> f32 {
+        let mut a = 0.0;
+        let mut i = self.init_vert;
+        loop {
+            let v1 = self.vertex(i);
+            i = v1.ccw_neib;
+            let v2 = self.vertex(i);
+            a += 0.5*v1.pos.cross(&v2.pos).norm();
+            if i == self.init_vert {break}
+        }
+        a
+    }
+
+    pub fn for_each_vert(&self, mut func: impl FnMut(&Vertex)) {
+        let mut i = self.init_vert;
+        loop {
+            let v = self.vertex(i);
+            func(v);
+            i = v.ccw_neib;
+            if i == self.init_vert {break}
+        }
+    }
+
 }
+
+pub struct VoronoiCellVertexIter<'a> {
+    cell: &'a VoronoiCell,
+    cur: usize,
+    stop: bool,
+}
+
+impl<'a> Iterator for VoronoiCellVertexIter<'a> {
+    type Item = &'a Vertex;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.stop {
+            None
+        } else {
+            let ret = &self.cell.vert[self.cur];
+            self.cur = ret.ccw_neib;
+            if self.cur == self.cell.init_vert {
+                self.stop = true;
+            }
+            Some(ret)
+        }
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -218,11 +265,15 @@ mod tests {
     #[test]
     fn cell_test() {
         let mut c = VoronoiCell::new(-10.0, 10.0, -10.0, 10.0);
-        c.add_point(&Vector2::new(20.0,-17.5));
-        c.add_point(&Vector2::new(10.0,0.0));
-        c.add_point(&Vector2::new(10.0,10.0));
-        c.add_point(&Vector2::new(-5.0*2.0,-2.0*2.0));
-        c.add_point(&Vector2::new(-5.0*2.0,5.0*2.0));
+        c.add_point(&Vector2::new(20.0,-17.5), 1);
+        c.add_point(&Vector2::new(10.0,0.0), 2);
+        c.add_point(&Vector2::new(10.0,10.0), 3);
+        c.add_point(&Vector2::new(-5.0*2.0,-2.0*2.0), 4);
+        c.add_point(&Vector2::new(-5.0*2.0,5.0*2.0), 5);
         c.write_to_file("tests/out.dat");
+
+        for v in c.iter_vertex() {
+            println!("{v:?}")
+        }
     }
 }
