8439deb9270cf22333fa0c875edaa2d2335e911b
yesint <yesint4@yahoo.com>
2024-07-02 09:12:44 +0200

    Adding an deletting atoms from Topology

--- src/core/topology.rs
+++ src/core/topology.rs
@@ -1,4 +1,5 @@
 use std::ops::Deref;
+use anyhow::bail;
 use sync_unsafe_cell::SyncUnsafeCell;
 
 use crate::io::TopologyProvider;
@@ -19,6 +20,7 @@ pub(crate) struct TopologyStorage {
 /// to be manipulated directly by the user. Insead [State](super::State) and [Topology] 
 /// are used to create atom selections, which give an access to the properties of
 /// individual atoms and allow to query various properties.
+#[derive(Default)]
 pub struct Topology(SyncUnsafeCell<TopologyStorage>);
 
 impl Clone for Topology {
@@ -81,6 +83,51 @@ impl Topology {
             self.get().atoms.len() == other.get().atoms.len()
         &&  self.get().bonds.len() == other.get().bonds.len()
         &&  self.get().molecules.len() == other.get().molecules.len()
+    }
+
+    pub fn add_atoms(&mut self, added: impl Iterator<Item = Atom>) {
+        self.get_mut().atoms.extend(added);
+    }
+
+    pub fn add_bonds(&mut self, added: impl Iterator<Item = [usize; 2]>) {
+        self.get_mut().bonds.extend(added);
+    }
+
+    pub fn add_molecules(&mut self, added: impl Iterator<Item = [usize; 2]>) {
+        self.get_mut().molecules.extend(added);
+    }
+
+    pub fn remove(&mut self, removed: impl Iterator<Item = usize>) -> anyhow::Result<()> {
+        let mut ind = removed.collect::<Vec<_>>();
+        if ind.len()==0 {
+            return Ok(());
+        }
+        ind.sort_unstable();
+        ind.dedup();
+        if ind[0] >= self.num_atoms() || ind[ind.len()-1] >= self.num_atoms() {
+            bail!(
+                "Indexes to remove [{}:{}] are out of allowed range [0:{}]",
+                ind[0],ind[ind.len()-1],self.num_atoms()
+            );
+        }
+        for i in ind.iter().rev().cloned() {
+            self.get_mut().atoms.remove(i);
+            // Remove affected bonds
+            self.get_mut().bonds.retain(|b| b[0] != i && b[1] != i);
+            // Modify molecules and remove those, which become invalid
+            self.get_mut().molecules.retain_mut(|m| {
+                if m[0]==i {m[0] += 1}
+                if m[1]==i {
+                    if m[1]>0 {
+                        m[0] -= 1
+                    } else {
+                        return false;
+                    }
+                }
+                m[1]>=m[0]
+            });
+        }
+        Ok(())
     }
 }
 
