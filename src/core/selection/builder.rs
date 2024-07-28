cf10c1af63716657ec50e67eaa64f887eed794a5
yesint <yesint4@yahoo.com>
2024-07-28 18:07:53 +0200

    Some more experiments with builder

--- src/core/selection/builder.rs
+++ src/core/selection/builder.rs
@@ -9,15 +9,12 @@ use super::utils::*;
 //-----------------------------------------------------
 
 
-pub struct BuilderSel {
-    system: Rc<RefCell<System>>,
+pub struct BuilderSel<'a> {
+    system: &'a RefMut<'a,System>,
     index: Vec<usize>,
 }
 
-pub struct BuilderSelGuard<'a>( Ref<'a,System> );
-pub struct BuilderSelGuardMut<'a>( RefMut<'a,System> );
-
-impl BuilderSel {
+impl BuilderSel<'_> {
     fn check_index(&self,n: usize) {
         if self.index[0] >= n || self.index[self.index.len()-1] >= n {
             panic!(
@@ -26,92 +23,46 @@ impl BuilderSel {
             );
         }
     }
-
-    pub fn guard(&self) -> BuilderSelGuard<'_> {
-        let g = self.system.borrow();        
-        let n = g.topology.num_atoms();
-        self.check_index(n);
-        BuilderSelGuard(g)
-    }
-
-    pub fn guard_mut(&self) -> BuilderSelGuardMut<'_> {
-        let g = self.system.borrow_mut();
-        let n = g.topology.num_atoms();
-        self.check_index(n);
-        BuilderSelGuardMut(g)
-    }
 }
 
 //---------------------------------------------
-impl PosProvider for BuilderSelGuard<'_> {
+impl PosProvider for BuilderSel<'_> {
     fn iter_pos(&self) -> impl PosIterator<'_> {
-        self.0.state.iter_pos()
+        unsafe { self.index.iter().map(|i| self.system.state.nth_pos_unchecked(*i)) }
     }
 }
 
-impl MeasurePos for BuilderSelGuard<'_> {}
+impl MeasurePos for BuilderSel<'_> {}
 
-impl AtomsProvider for BuilderSelGuard<'_> {
+impl AtomsProvider for BuilderSel<'_> {
     fn iter_atoms(&self) -> impl AtomIterator<'_> {
-        self.0.topology.iter_atoms()
+        unsafe {
+            self.index
+                .iter()
+                .map(|i| self.system.topology.nth_atom_unchecked(*i))
+        }
     }
 }
 
-impl TopologyProvider for BuilderSelGuard<'_> {
+impl TopologyProvider for BuilderSel<'_> {
     fn num_atoms(&self) -> usize {
-        self.0.topology.num_atoms()
+        self.system.topology.num_atoms()
     }
 }
 
-impl BoxProvider for BuilderSelGuard<'_> {
+impl BoxProvider for BuilderSel<'_> {
     fn get_box(&self) -> Option<&PeriodicBox> {
-        self.0.state.get_box()
+        self.system.state.get_box()
     }
 }
 
-impl StateProvider for BuilderSelGuard<'_> {
+impl StateProvider for BuilderSel<'_> {
     fn get_time(&self) -> f32 {
-        self.0.state.get_time()
+        self.system.state.get_time()
     }
 
     fn num_coords(&self) -> usize {
-        self.0.state.num_coords()
-    }
-}
-//---------------------------------------------
-impl PosProvider for BuilderSelGuardMut<'_> {
-    fn iter_pos(&self) -> impl PosIterator<'_> {
-        self.0.state.iter_pos()
-    }
-}
-
-impl MeasurePos for BuilderSelGuardMut<'_> {}
-
-impl AtomsProvider for BuilderSelGuardMut<'_> {
-    fn iter_atoms(&self) -> impl AtomIterator<'_> {
-        self.0.topology.iter_atoms()
-    }
-}
-
-impl TopologyProvider for BuilderSelGuardMut<'_> {
-    fn num_atoms(&self) -> usize {
-        self.0.topology.num_atoms()
-    }
-}
-
-impl BoxProvider for BuilderSelGuardMut<'_> {
-    fn get_box(&self) -> Option<&PeriodicBox> {
-        self.0.state.get_box()
-    }
-}
-
-impl StateProvider for BuilderSelGuardMut<'_> {
-    fn get_time(&self) -> f32 {
-        self.0.state.get_time()
-    }
-
-    fn num_coords(&self) -> usize {
-        self.0.state.num_coords()
+        self.system.state.num_coords()
     }
 }
 //----------------------------------------------
@@ -119,6 +70,15 @@ impl StateProvider for BuilderSelGuardMut<'_> {
 pub struct Builder {
     system: Rc<RefCell<System>>,
 }
+pub struct BuilderGuard<'a>{
+    sys_ref: Ref<'a,System>,
+    sys_rc: &'a Rc<RefCell<System>>,
+}
+pub struct BuilderGuardMut<'a>{
+    sys_ref: RefMut<'a,System>,
+    sys_rc: &'a Rc<RefCell<System>>,
+}
+
 
 impl Builder {
     pub fn new(topology: Topology,state: State) -> Result<Self> {
@@ -127,60 +87,66 @@ impl Builder {
         })
     }
 
+    pub fn guard(&self) -> BuilderGuard<'_> {
+        let g = self.system.borrow();
+        BuilderGuard{sys_ref: g, sys_rc: &self.system}
+    }
+
+    pub fn guard_mut(&self) -> BuilderGuardMut<'_> {
+        let g = self.system.borrow_mut();
+        BuilderGuardMut{sys_ref: g, sys_rc: &self.system}
+    }
+}
+
+impl BuilderGuardMut<'_> {
     pub fn select_all(&self) -> Result<BuilderSel> {
-        let guard = self.system.borrow();
-        let vec = index_from_all(guard.topology.num_atoms());
+        let vec = index_from_all(self.sys_ref.topology.num_atoms());
         Ok(
             BuilderSel {
-                system: Rc::clone(&self.system),
+                system: &self.sys_ref,
                 index: vec,
             }
         )
     }
 
     pub fn select_str(&self, selstr: &str) -> Result<BuilderSel> {
-        let guard = self.system.borrow();
-        let vec = index_from_str(selstr, &guard.topology, &guard.state)?;
+        let vec = index_from_str(selstr, &self.sys_ref.topology, &self.sys_ref.state)?;
         Ok(
             BuilderSel {
-                system: Rc::clone(&self.system),
+                system: &self.sys_ref,
                 index: vec,
             }
         )
     }
 
     pub fn select_expr(&self, expr: &SelectionExpr) -> Result<BuilderSel> {
-        let guard = self.system.borrow();
-        let vec = index_from_expr(expr, &guard.topology, &guard.state)?;
+        let vec = index_from_expr(expr, &self.sys_ref.topology, &self.sys_ref.state)?;
         Ok(
             BuilderSel {
-                system: Rc::clone(&self.system),
+                system: &self.sys_ref,
                 index: vec,
             }
         )
     }
 
     pub fn select_range(&self, range: &std::ops::Range<usize>) -> Result<BuilderSel> {
-        let guard = self.system.borrow();
-        let vec = index_from_range(range, guard.topology.num_atoms())?;
+        let vec = index_from_range(range, self.sys_ref.topology.num_atoms())?;
         Ok(
             BuilderSel {
-                system: Rc::clone(&self.system),
+                system: &self.sys_ref,
                 index: vec,
             }
         )
     }
 
-    pub fn append(&mut self, data: &(impl TopologyProvider + StateProvider)) {
-        let guard = self.system.borrow_mut();
-        guard.topology.get_storage_mut().add_atoms(data.iter_atoms());
-        guard.state.get_storage_mut().add_coords(data.iter_pos());
+    pub fn append(&self, data: &(impl TopologyProvider + StateProvider)) {
+        self.sys_ref.topology.get_storage_mut().add_atoms(data.iter_atoms());
+        self.sys_ref.state.get_storage_mut().add_coords(data.iter_pos());
     }
 
-    pub fn remove(&mut self, to_remove: &impl IndexProvider) -> Result<()> {
-        let guard = self.system.borrow_mut();
-        guard.topology.get_storage_mut().remove_atoms(to_remove.iter_index())?;
-        guard.state.get_storage_mut().remove_coords(to_remove.iter_index())?;
+    pub fn remove(&self, to_remove: &impl IndexProvider) -> Result<()> {
+        self.sys_ref.topology.get_storage_mut().remove_atoms(to_remove.iter_index())?;
+        self.sys_ref.state.get_storage_mut().remove_coords(to_remove.iter_index())?;
         Ok(())
     }
 
@@ -198,9 +164,10 @@ mod tests {
         let (top,st) = FileHandler::open("tests/protein.pdb")?.read()?;
         
         let mut bu = Builder::new(top,st)?;
-        let sel1 = bu.select_str("index 1:20")?;
-        let cm = sel1.guard().center_of_geometry();
-        //bu.append(&sel1);
+        let g = bu.guard_mut();
+        let sel1 = g.select_str("index 1:20")?;
+        let cm = sel1.center_of_geometry();
+        g.append(&sel1);
         Ok(())
     }
 }
