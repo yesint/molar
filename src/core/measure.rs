af1956177115e3f02ed981dae2a7112b95bc6313
yesint <yesint4@yahoo.com>
2024-08-06 13:46:02 +0200

    Custom errors in Measure

--- src/core/measure.rs
+++ src/core/measure.rs
@@ -1,16 +1,28 @@
-use std::iter::zip;
 use super::providers::*;
 use super::Matrix3f;
 use super::Pos;
 use super::Vector3f;
-use anyhow::{bail,Result,anyhow};
 use itertools::izip;
 use nalgebra::SymmetricEigen;
-use nalgebra::{IsometryMatrix3,Rotation3,Translation3};
+use nalgebra::{IsometryMatrix3, Rotation3, Translation3};
 use num_traits::Bounded;
+use std::iter::zip;
+use thiserror::Error;
 //==============================================================
 // Traits for measuring (immutable access)
 //==============================================================
+
+#[derive(Error, Debug)]
+pub enum MeasureError {
+    #[error("invalid sizes: {0} and {1}")]
+    Sizes(usize, usize),
+    #[error("zero mass")]
+    ZeroMass,
+    #[error("SVD failed")]
+    Svd,
+    #[error("no periodic box")]
+    NoPbc,
+}
 
 /// Trait for analysis requiring only positions
 pub trait MeasurePos: PosProvider {
@@ -40,22 +52,18 @@ pub trait MeasurePos: PosProvider {
         Pos::from(cog / n as f32)
     }
 
-    fn rmsd(sel1: &Self, sel2: &Self) -> Result<f32> {
+    fn rmsd(sel1: &Self, sel2: &Self) -> Result<f32, MeasureError> {
         let mut res = 0.0;
         let iter1 = sel1.iter_pos();
         let iter2 = sel2.iter_pos();
 
         if iter1.len() != iter2.len() {
-            bail!(
-                "Different sizes in rmsd: {} and {}",
-                iter1.len(),
-                iter2.len()
-            );
+            return Err(MeasureError::Sizes(iter1.len(), iter2.len()));
         }
 
         let n = iter1.len();
         if n == 0 {
-            bail!("No atoms in rmsd")
+            return Err(MeasureError::Sizes(iter1.len(), iter2.len()));
         }
 
         for (p1, p2) in std::iter::zip(iter1, iter2) {
@@ -68,7 +76,7 @@ pub trait MeasurePos: PosProvider {
 
 /// Trait for analysis requiring positions and masses
 pub trait MeasureMasses: PosProvider + MassesProvider {
-    fn center_of_mass(&self) -> Result<Pos> {
+    fn center_of_mass(&self) -> Result<Pos, MeasureError> {
         let mut cm = Vector3f::zeros();
         let mut mass = 0.0;
         for (c, m) in zip(self.iter_pos(), self.iter_masses()) {
@@ -77,13 +85,16 @@ pub trait MeasureMasses: PosProvider + MassesProvider {
         }
 
         if mass == 0.0 {
-            bail!("Zero mass in COM!")
+            Err(MeasureError::ZeroMass)
         } else {
             Ok(Pos::from(cm / mass))
         }
     }
 
-    fn fit_transform(sel1: &Self, sel2: &Self) -> Result<nalgebra::IsometryMatrix3<f32>> {
+    fn fit_transform(
+        sel1: &Self,
+        sel2: &Self,
+    ) -> Result<nalgebra::IsometryMatrix3<f32>, MeasureError> {
         let cm1 = sel1.center_of_mass()?;
         let cm2 = sel2.center_of_mass()?;
 
@@ -92,32 +103,31 @@ pub trait MeasureMasses: PosProvider + MassesProvider {
             sel1.iter_pos().map(|p| *p - cm1),
             sel2.iter_pos().map(|p| *p - cm2),
             sel1.iter_masses(),
-        );
+        )?;
 
         Ok(Translation3::from(cm2) * rot * Translation3::from(-cm1))
     }
 
-    fn fit_transform_at_origin(sel1: &Self, sel2: &Self) -> Result<nalgebra::IsometryMatrix3<f32>> {
+    fn fit_transform_at_origin(
+        sel1: &Self,
+        sel2: &Self,
+    ) -> Result<nalgebra::IsometryMatrix3<f32>, MeasureError> {
         let rot = rot_transform(
             sel1.iter_pos().map(|p| p.coords),
             sel2.iter_pos().map(|p| p.coords),
             sel1.iter_masses(),
-        );
+        )?;
         Ok(nalgebra::convert(rot))
     }
 
-    fn rmsd_mw(sel1: &Self, sel2: &Self) -> Result<f32> {
+    fn rmsd_mw(sel1: &Self, sel2: &Self) -> Result<f32, MeasureError> {
         let mut res = 0.0;
         let mut m_tot = 0.0;
         let iter1 = sel1.iter_pos();
         let iter2 = sel2.iter_pos();
 
         if iter1.len() != iter2.len() {
-            bail!(
-                "Different sizes in rmsd_mw: {} and {}",
-                iter1.len(),
-                iter2.len()
-            );
+            return Err(MeasureError::Sizes(iter1.len(), iter2.len()));
         }
 
         for (p1, p2, m) in izip!(iter1, iter2, sel1.iter_masses()) {
@@ -126,42 +136,33 @@ pub trait MeasureMasses: PosProvider + MassesProvider {
         }
 
         if m_tot == 0.0 {
-            bail!("Zero mass in rmsd_mw")
+            Err(MeasureError::ZeroMass)
         } else {
             Ok((res / m_tot).sqrt())
         }
     }
 
-    fn gyration(&self) -> Result<f32> {
+    fn gyration(&self) -> Result<f32, MeasureError> {
         let c = self.center_of_mass()?;
-        do_gyration(
-            self.iter_pos().map(|pos| pos-c), 
-            self.iter_masses()
-        )
+        Ok(do_gyration(self.iter_pos().map(|pos| pos - c), self.iter_masses()))
     }
 
-    fn inertia(&self) -> Result<(Vector3f,Matrix3f)> {
+    fn inertia(&self) -> Result<(Vector3f, Matrix3f), MeasureError> {
         let c = self.center_of_mass()?;
-        do_inertia(
-            self.iter_pos().map(|pos| pos-c), 
-            self.iter_masses()
-        )
+        Ok(do_inertia(self.iter_pos().map(|pos| pos - c), self.iter_masses()))
     }
 
-    fn principal_transform(&self) -> Result<IsometryMatrix3<f32>> {
+    fn principal_transform(&self) -> Result<IsometryMatrix3<f32>, MeasureError> {
         let c = self.center_of_mass()?;
-        let (_,axes) = do_inertia(
-            self.iter_pos().map(|pos| pos-c), 
-            self.iter_masses()
-        )?;
-        Ok( do_principal_transform(axes, c.coords) )
+        let (_, axes) = do_inertia(self.iter_pos().map(|pos| pos - c), self.iter_masses());
+        Ok(do_principal_transform(axes, c.coords))
     }
 }
 
 /// Trait for analysis requiring positions, masses and pbc
 pub trait MeasurePeriodic: PosProvider + MassesProvider + BoxProvider {
-    fn center_of_mass_pbc(&self) -> Result<Pos> {
-        let b = self.get_box().ok_or_else(|| anyhow!("No periodicity!"))?;
+    fn center_of_mass_pbc(&self) -> Result<Pos, MeasureError> {
+        let b = self.get_box().ok_or_else(|| MeasureError::NoPbc)?;
         let mut pos_iter = self.iter_pos();
         let mut mass_iter = self.iter_masses();
 
@@ -176,86 +177,92 @@ pub trait MeasurePeriodic: PosProvider + MassesProvider + BoxProvider {
         }
 
         if mass == 0.0 {
-            bail!("Zero mass in COM!")
+            Err(MeasureError::ZeroMass)
         } else {
             Ok(Pos::from(cm / mass))
         }
     }
 
-    fn gyration_pbc(&self) -> Result<f32> {
-        let b = self.get_box().ok_or_else(|| anyhow!("No periodicity!"))?;
+    fn gyration_pbc(&self) -> Result<f32, MeasureError> {
+        let b = self.get_box().ok_or_else(|| MeasureError::NoPbc)?;
         let c = self.center_of_mass_pbc()?;
-        do_gyration(
-            self.iter_pos().map(|pos| b.shortest_vector(&(pos-c))),
-            self.iter_masses()
-        )
+        Ok(do_gyration(
+            self.iter_pos().map(|pos| b.shortest_vector(&(pos - c))),
+            self.iter_masses(),
+        ))
     }
 
-    fn inertia_pbc(&self) -> Result<(Vector3f,Matrix3f)> {
-        let b = self.get_box().ok_or_else(|| anyhow!("No periodicity!"))?;
+    fn inertia_pbc(&self) -> Result<(Vector3f, Matrix3f), MeasureError> {
+        let b = self.get_box().ok_or_else(|| MeasureError::NoPbc)?;
         let c = self.center_of_mass_pbc()?;
-        do_inertia(
-            self.iter_pos().map(|pos| b.shortest_vector(&(pos-c))), 
-            self.iter_masses()
-        )
+        Ok(do_inertia(
+            self.iter_pos().map(|pos| b.shortest_vector(&(pos - c))),
+            self.iter_masses(),
+        ))
     }
 
-    fn principal_transform_pbc(&self) -> Result<IsometryMatrix3<f32>> {
-        let b = self.get_box().ok_or_else(|| anyhow!("No periodicity!"))?;
+    fn principal_transform_pbc(&self) -> Result<IsometryMatrix3<f32>, MeasureError> {
+        let b = self.get_box().ok_or_else(|| MeasureError::NoPbc)?;
         let c = self.center_of_mass_pbc()?;
-        let (_,axes) = do_inertia(
-            self.iter_pos().map(|pos| b.shortest_vector(&(pos-c))), 
-            self.iter_masses()
-        )?;
-        Ok( do_principal_transform(axes, c.coords) )
+        let (_, axes) = do_inertia(
+            self.iter_pos().map(|pos| b.shortest_vector(&(pos - c))),
+            self.iter_masses(),
+        );
+        Ok(do_principal_transform(axes, c.coords))
     }
 }
 
-fn do_gyration(dists: impl Iterator<Item = Vector3f>, masses: impl Iterator<Item = f32>) -> Result<f32> {
+fn do_gyration(
+    dists: impl Iterator<Item = Vector3f>,
+    masses: impl Iterator<Item = f32>,
+) -> f32 {
     let mut sd = 0.0;
     let mut sm = 0.0;
-    for (d,m) in zip(dists,masses) {
+    for (d, m) in zip(dists, masses) {
         sd += d.norm_squared() * m;
         sm += m;
     }
     // Don't need to test for zero mass since this is already done in center_of_mass()
-    Ok( (sd/sm).sqrt() )
+    (sd / sm).sqrt()
 }
 
-fn do_inertia(dists: impl Iterator<Item = Vector3f>, masses: impl Iterator<Item = f32>) -> Result<(Vector3f,Matrix3f)> {
+fn do_inertia(
+    dists: impl Iterator<Item = Vector3f>,
+    masses: impl Iterator<Item = f32>,
+) -> (Vector3f, Matrix3f) {
     let mut tens = Matrix3f::zeros();
-    
-    for (d,m) in zip(dists,masses) {
-        tens[(0,0)] += m*( d.y*d.y + d.z*d.z );
-        tens[(1,1)] += m*( d.x*d.x + d.z*d.z );
-        tens[(2,2)] += m*( d.x*d.x + d.y*d.y );
-        tens[(0,1)] -= m*d.x*d.y;
-        tens[(0,2)] -= m*d.x*d.z;
-        tens[(1,2)] -= m*d.y*d.z;
+
+    for (d, m) in zip(dists, masses) {
+        tens[(0, 0)] += m * (d.y * d.y + d.z * d.z);
+        tens[(1, 1)] += m * (d.x * d.x + d.z * d.z);
+        tens[(2, 2)] += m * (d.x * d.x + d.y * d.y);
+        tens[(0, 1)] -= m * d.x * d.y;
+        tens[(0, 2)] -= m * d.x * d.z;
+        tens[(1, 2)] -= m * d.y * d.z;
     }
-    tens[(1,0)] = tens[(0,1)];
-    tens[(2,0)] = tens[(0,2)];
-    tens[(2,1)] = tens[(1,2)];
-    
+    tens[(1, 0)] = tens[(0, 1)];
+    tens[(2, 0)] = tens[(0, 2)];
+    tens[(2, 1)] = tens[(1, 2)];
+
     // Compute axes and moments
     let eig = SymmetricEigen::new(tens);
     // Eigenvectors are NOT sorted, so sort them manually
-    let mut s = eig.eigenvalues.into_iter().enumerate().collect::<Vec<(_,_)>>();
-    s.sort_unstable_by(|a,b| a.1.partial_cmp(b.1).unwrap());
+    let mut s = eig
+        .eigenvalues
+        .into_iter()
+        .enumerate()
+        .collect::<Vec<(_, _)>>();
+    s.sort_unstable_by(|a, b| a.1.partial_cmp(b.1).unwrap());
     // Create reordered moments and axes
-    let moments = Vector3f::new(
-        *s[0].1, *s[1].1, *s[2].1
-    );
+    let moments = Vector3f::new(*s[0].1, *s[1].1, *s[2].1);
 
     let col0 = eig.eigenvectors.column(s[0].0).normalize();
     let col1 = eig.eigenvectors.column(s[1].0).normalize();
     let col2 = col0.cross(&col1); // Ensure right-handed system
-    
-    let axes = Matrix3f::from_columns(
-        &[col0,col1,col2]
-    );
 
-    Ok((moments,axes))
+    let axes = Matrix3f::from_columns(&[col0, col1, col2]);
+
+    (moments, axes)
 }
 
 // Straighforward implementation of Kabsch algorithm
@@ -263,7 +270,7 @@ pub fn rot_transform(
     pos1: impl Iterator<Item = Vector3f>,
     pos2: impl Iterator<Item = Vector3f>,
     masses: impl Iterator<Item = f32>,
-) -> Rotation3<f32> {
+) -> Result<Rotation3<f32>, MeasureError> {
     //Calculate the covariance matrix
     let mut cov = Matrix3f::zeros();
 
@@ -273,8 +280,8 @@ pub fn rot_transform(
 
     // Perform Singular Value Decomposition (SVD) on the covariance matrix
     let svd = nalgebra::SVD::new(cov, true, true);
-    let u = svd.u.unwrap();
-    let v_t = svd.v_t.unwrap();
+    let u = svd.u.ok_or_else(|| MeasureError::Svd)?;
+    let v_t = svd.v_t.ok_or_else(|| MeasureError::Svd)?;
 
     // Determine if a reflection is necessary
     let d = if (u * v_t).determinant() < 0.0 {
@@ -288,14 +295,12 @@ pub fn rot_transform(
     d_matrix[(2, 2)] = d;
 
     // Compute the optimal rotation matrix
-    Rotation3::from_matrix_unchecked(u * d_matrix * v_t)
+    Ok(Rotation3::from_matrix_unchecked(u * d_matrix * v_t))
 }
 
 // Principal transform algorithms
 fn do_principal_transform(mut axes: Matrix3f, cm: Vector3f) -> IsometryMatrix3<f32> {
     axes.try_inverse_mut();
-    
-    Translation3::from(cm) 
-    * Rotation3::from_matrix_unchecked(axes) 
-    * Translation3::from(-cm)
+
+    Translation3::from(cm) * Rotation3::from_matrix_unchecked(axes) * Translation3::from(-cm)
 }
