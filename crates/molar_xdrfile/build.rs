9cb672d47c4ada9ad044b03a4c1d94187b511bfa
yesint <yesint4@yahoo.com>
2024-10-10 20:52:45 +0200

    Switching to cc crate, removed cmake dependency, removed in place gromacs build

--- crates/molar_xdrfile/build.rs
+++ crates/molar_xdrfile/build.rs
@@ -1,7 +1,12 @@
-use cmake::Config;
-
 fn main() {
-    let dst = Config::new(format!("xdrfile")).profile("Release").build();
-    println!("cargo:rustc-link-search=native={}", dst.display());
-    println!("cargo:rustc-link-lib=static=xdrfile");
+    cc::Build::new()
+    .file("xdrfile/xdrfile.c")
+    .file("xdrfile/xdrfile_xtc.c")
+    .file("xdrfile/xdrfile_trr.c")
+    .file("xdrfile/xdr_utils.c")
+    .file("xdrfile/xdr_seek.c")
+    .include("xdrfile")
+    .pic(true)
+    .warnings(false)
+    .compile("xdrfile");
 }
