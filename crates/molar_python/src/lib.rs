abfb20f479c84cd149a7a7695706d2bea0bd71e2
yesint <yesint4@yahoo.com>
2024-12-25 15:16:39 +0100

    Add Atom class with properties and methods for Python bindings

--- crates/molar_python/src/lib.rs
+++ crates/molar_python/src/lib.rs
@@ -1,9 +1,10 @@
 use molar::prelude::*;
 use numpy::{
-    nalgebra, npyffi::{self}, PyArray1, PyArrayMethods, PyReadonlyArray1, ToNpyDims, PY_ARRAY_API
+    npyffi::{self},
+    PyArray1, PyArrayMethods, PyReadonlyArray1, ToNpyDims, PY_ARRAY_API,
 };
 use pyo3::{prelude::*, IntoPyObjectExt};
-use std::{borrow::Borrow, ffi::c_void};
+use std::ffi::c_void;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -13,6 +14,137 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 
 #[pyclass]
 struct Atom(molar::core::Atom);
+
+#[pymethods]
+impl Atom {
+    #[new]
+    fn new() -> Self {
+        Self {
+            0: Default::default(),
+        }
+    }
+
+    // name
+    #[getter(name)]
+    fn get_name(&self, _py: Python) -> &str {
+        self.0.name.as_str()
+    }
+
+    #[setter(name)]
+    fn set_name(&mut self, value: &str) {
+        self.0.name = value.into();
+    }
+
+    // resname
+    #[getter(resname)]
+    fn get_resname(&self, _py: Python) -> &str {
+        self.0.resname.as_str()
+    }
+
+    #[setter(resname)]
+    fn set_resname(&mut self, value: &str) {
+        self.0.resname = value.into();
+    }
+
+    // resid
+    #[getter(resname)]
+    fn get_resid(&self, _py: Python) -> i32 {
+        self.0.resid
+    }
+
+    #[setter(resname)]
+    fn set_resid(&mut self, value: i32) {
+        self.0.resid = value;
+    }
+
+    // atomic_number
+    #[getter(atomic_number)]
+    fn get_atomic_number(&self, _py: Python) -> u8 {
+        self.0.atomic_number
+    }
+
+    #[setter(atomic_number)]
+    fn set_atomic_number(&mut self, value: u8) {
+        self.0.atomic_number = value;
+    }
+
+    // mass
+    #[getter(mass)]
+    fn get_mass(&self, _py: Python) -> f32 {
+        self.0.mass
+    }
+
+    #[setter(mass)]
+    fn set_mass(&mut self, value: f32) {
+        self.0.mass = value;
+    }
+
+    // charge
+    #[getter(charge)]
+    fn get_charge(&self, _py: Python) -> f32 {
+        self.0.charge
+    }
+
+    #[setter(charge)]
+    fn set_charge(&mut self, value: f32) {
+        self.0.charge = value;
+    }
+
+    // type_name
+    #[getter(type_name)]
+    fn get_type_name(&self, _py: Python) -> &str {
+        self.0.type_name.as_str()
+    }
+
+    #[setter(type_name)]
+    fn set_type_name(&mut self, value: &str) {
+        self.0.type_name = value.into();
+    }
+
+    // type_id
+    #[getter(type_id)]
+    fn get_type_id(&self, _py: Python) -> u32 {
+        self.0.type_id
+    }
+
+    #[setter(type_id)]
+    fn set_type_id(&mut self, value: u32) {
+        self.0.type_id = value;
+    }
+
+    // chain
+    #[getter(chain)]
+    fn get_chain(&self, _py: Python) -> char {
+        self.0.chain
+    }
+
+    #[setter(chain)]
+    fn set_chain(&mut self, value: char) {
+        self.0.chain = value;
+    }
+
+    // bfactor
+    #[getter(bfactor)]
+    fn get_bfactor(&self, _py: Python) -> f32 {
+        self.0.bfactor
+    }
+
+    #[setter(bfactor)]
+    fn set_bfactor(&mut self, value: f32) {
+        self.0.bfactor = value;
+    }
+
+    // occupancy
+    #[getter(occupancy)]
+    fn get_occupancy(&self, _py: Python) -> f32 {
+        self.0.occupancy
+    }
+
+    #[setter(occupancy)]
+    fn set_occupancy(&mut self, value: f32) {
+        self.0.occupancy = value;
+    }
+}
 
 #[pyclass(unsendable)]
 struct Particle {
@@ -25,17 +157,29 @@ struct Particle {
 
 #[pymethods]
 impl Particle {
+    //pos
     #[getter]
     fn get_pos<'py>(&mut self, py: Python<'py>) -> Bound<'py, PyAny> {
         map_pos_to_pyarray(py, self.pos)
     }
 
     #[setter]
-    fn set_pos<'py>(&mut self, value: PyReadonlyArray1<'py,f32>) -> PyResult<()> {
+    fn set_pos<'py>(&mut self, value: PyReadonlyArray1<'py, f32>) -> PyResult<()> {
         unsafe {
             std::ptr::copy_nonoverlapping(value.data(), self.pos.coords.as_mut_ptr(), 3);
         }
         Ok(())
+    }
+
+    // atom
+    #[getter(atom)]
+    fn get_atom(&self, _py: Python) -> Atom {
+        Atom(self.atom.clone())
+    }
+
+    #[setter(atom)]
+    fn set_atom(&mut self, value: &Atom) {
+        *self.atom = value.0.clone();
     }
 
     // name
@@ -220,80 +364,78 @@ impl Source {
 }
 
 //====================================
-#[derive(FromPyObject)]
-enum SliceOrInt{
-    Slice(Py<pyo3::types::PySlice>),
-    Int(isize),
-}
 
 #[pyclass(unsendable)]
 struct Sel(molar::core::Sel<MutableSerial>);
 
 #[pymethods]
 impl Sel {
-    fn len(&self) -> usize {
-        self.0.len()
-    }
-
     fn __len__(&self) -> usize {
         self.0.len()
     }
 
-    fn __getitem__(slf: Bound<Self>, ind: SliceOrInt) -> PyResult<Py<PyAny>> {
+    // Indexing
+    fn __getitem__(slf: Bound<Self>, i: isize) -> PyResult<Py<PyAny>> {
         let s = slf.borrow();
-        match ind {
-            SliceOrInt::Int(i) => {
-                let ind = if i<0 {
-                    s.len() - i.unsigned_abs()
-                } else {
-                    i as usize
-                };
-                s.nth_particle(ind).map(|p| p.into_py_any(slf.py()).unwrap())
-            },
-            SliceOrInt::Slice(s) => {
-                todo!("subselect here");
-                Ok(s.into_py_any(slf.py()).unwrap())
+        let ind = if i < 0 {
+            if i.abs() > s.__len__() as isize {
+                return Err(anyhow::anyhow!(
+                    "Negative index {i} is out of bounds {}:-1",
+                    -(s.__len__() as isize)
+                )
+                .into());
             }
+            s.__len__() - i.unsigned_abs()
+        } else {
+            i as usize
+        };
+
+        let p =
+            s.0.nth_particle_mut(ind)
+                .ok_or_else(|| anyhow::anyhow!("Index {} is out of bounds 0:{}", i, s.__len__()))?;
+        let atom_ptr = p.atom as *mut molar::core::Atom;
+        let pos_ptr = p.pos as *mut molar::core::Pos;
+        Ok(Particle {
+            atom: unsafe { &mut *atom_ptr },
+            pos: unsafe { &mut *pos_ptr },
+            id: p.id,
         }
-        
+        .into_py_any(slf.py())?)
     }
 
     fn com<'a>(&self, _py: Python<'a>) -> PyResult<Bound<'a, numpy::PyArray1<f32>>> {
         Ok(copy_pos_to_pyarray(
             _py,
             &self.0.center_of_mass().map_err(|e| anyhow::anyhow!(e))?,
-        )) //.into_py(_py))
+        ))
     }
 
     fn nth_pos<'a>(slf: Bound<Self>, py: Python<'a>, i: usize) -> PyResult<Bound<'a, PyAny>> {
         let s = slf.borrow();
-        let pos = s.0.nth_pos_mut(i).ok_or_else(|| anyhow::anyhow!("Out of bounds"))?;
+        let pos =
+            s.0.nth_pos_mut(i)
+                .ok_or_else(|| anyhow::anyhow!("Out of bounds"))?;
         Ok(map_pos_to_pyarray(py, pos))
     }
 
-    fn nth_particle(&self, i: usize) -> PyResult<Particle> {
-        let p = self.0.nth_particle_mut(i).ok_or_else(|| anyhow::anyhow!("Out of bounds"))?;
-        let arom_ptr = p.atom as *mut molar::core::Atom;
-        let pos_ptr = p.pos as *mut molar::core::Pos;
-        Ok(Particle{
-            atom: unsafe {&mut *arom_ptr},
-            pos: unsafe {&mut *pos_ptr},
-            id: p.id,
-        })
-    }
-
+    // Iteration protocol
     fn __iter__(slf: PyRef<'_, Self>) -> PyRef<'_, ParticleIterator> {
-        Bound::new(slf.py(),ParticleIterator{
-            sel: slf.into(),
-            cur: 0,
-        }).unwrap().borrow()
+        Bound::new(
+            slf.py(),
+            ParticleIterator {
+                sel: slf.into(),
+                cur: 0,
+            },
+        )
+        .unwrap()
+        .borrow()
     }
 }
 
 #[pyclass]
 struct ParticleIterator {
     sel: Py<Sel>,
-    cur: usize,
+    cur: isize,
 }
 
 #[pymethods]
@@ -303,16 +445,15 @@ impl ParticleIterator {
     }
 
     fn __next__(mut slf: PyRefMut<'_, Self>) -> Option<PyObject> {
-        let ret = Python::with_gil(|py|{
-            slf.sel.borrow(py).nth_particle(slf.cur).ok().map(|p|{
-                p.into_py_any(py).unwrap()
-            })
-        });
+        let ret = Python::with_gil(|py| {
+            let s = slf.sel.bind(py);
+            Sel::__getitem__(s.clone(), slf.cur)
+        })
+        .ok();
         slf.cur += 1;
         ret
     }
 }
-
 
 // Constructs PyArray backed by existing Pos data.
 fn map_pos_to_pyarray<'py>(py: Python<'py>, data: &mut molar::core::Pos) -> Bound<'py, PyAny> {
@@ -328,11 +469,11 @@ fn map_pos_to_pyarray<'py>(py: Python<'py>, data: &mut molar::core::Pos) -> Boun
             f32::get_dtype(py).into_dtype_ptr(),
             dims.ndim_cint(),
             dims.as_dims_ptr(),
-            std::ptr::null_mut(),                 // no strides
+            std::ptr::null_mut(),                    // no strides
             data.coords.as_mut_ptr() as *mut c_void, // data
             //npyffi::NPY_ARRAY_WRITEABLE | npyffi::NPY_ARRAY_F_CONTIGUOUS, // flag
             npyffi::NPY_ARRAY_F_CONTIGUOUS, // flag
-            std::ptr::null_mut(),             // obj
+            std::ptr::null_mut(),           // obj
         );
         Bound::from_borrowed_ptr(py, ptr)
     }
@@ -347,35 +488,6 @@ fn copy_pos_to_pyarray<'py>(py: Python<'py>, data: &molar::core::Pos) -> Bound<'
     }
 }
 
-// unsafe fn pyarray_from_pos<'py>(
-//     py: Python<'py>,
-//     data: &mut Pos,
-//     container: *mut PyAny,
-// ) -> Bound<'py, Self>
-// where
-//     ID: IntoDimension<Dim = D>,
-// {
-//     let mut dims = dims.into_dimension();
-//     let ptr = PY_ARRAY_API.PyArray_NewFromDescr(
-//         py,
-//         PY_ARRAY_API.get_type_object(py, npyffi::NpyTypes::PyArray_Type),
-//         T::get_dtype(py).into_dtype_ptr(),
-//         dims.ndim_cint(),
-//         dims.as_dims_ptr(),
-//         strides as *mut npy_intp,    // strides
-//         data_ptr as *mut c_void,     // data
-//         npyffi::NPY_ARRAY_WRITEABLE, // flag
-//         ptr::null_mut(),             // obj
-//     );
-
-//     PY_ARRAY_API.PyArray_SetBaseObject(
-//         py,
-//         ptr as *mut npyffi::PyArrayObject,
-//         container as *mut ffi::PyObject,
-//     );
-
-//     Bound::from_owned_ptr(py, ptr).downcast_into_unchecked()
-// }
 //====================================
 
 /// A Python module implemented in Rust.
