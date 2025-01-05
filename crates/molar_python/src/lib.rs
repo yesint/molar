a3b8f4151c7c127c92f9f3a1f5ed4aadcc66dc20
yesint <yesint4@yahoo.com>
2025-01-05 14:16:47 +0100

    More work on bindings

--- crates/molar_python/src/lib.rs
+++ crates/molar_python/src/lib.rs
@@ -1,84 +1,23 @@
-use anyhow::bail;
+use anyhow::anyhow;
 use molar::prelude::*;
 use numpy::{
-    nalgebra, npyffi, AllowTypeChange, PyArray1, PyArrayLike1, PyArrayLike2, PyArrayMethods, PyReadonlyArray2, PyUntypedArrayMethods, ToNpyDims, PY_ARRAY_API
+    npyffi, PyArrayMethods, PyReadonlyArray2, PyUntypedArrayMethods, ToNpyDims, PY_ARRAY_API,
 };
-use pyo3::{
-    prelude::*,
-    types::{PyList, PyString, PyTuple},
-    IntoPyObjectExt,
-};
-use std::ffi::c_void;
+use pyo3::{prelude::*, types::PyTuple, IntoPyObjectExt};
 
-pub mod atom;
+mod utils;
+use utils::*;
+
+mod atom;
 use atom::Atom;
 
-pub mod particle;
+mod particle;
 use particle::Particle;
 
+mod periodic_box;
+use periodic_box::PeriodicBox;
+
 //-------------------------------------------
-#[pyclass]
-struct PeriodicBox(molar::core::PeriodicBox);
-
-#[pymethods]
-impl PeriodicBox {
-    #[staticmethod]
-    fn from_matrix<'py>(arr: PyArrayLike2<'py, f32, AllowTypeChange>) -> anyhow::Result<Self> {
-        if arr.shape() != [3, 3] {
-            bail!("Matrix must be 3x3");
-        }
-
-        let mut m = Matrix3f::zeros();
-        unsafe {
-            std::ptr::copy_nonoverlapping(arr.data(), m.as_mut_ptr(), 9);
-        }
-        Ok(PeriodicBox(molar::core::PeriodicBox::from_matrix(m)?))
-    }
-
-    #[staticmethod]
-    fn from_vectors_angles<'py>(
-        v_arr: PyArrayLike1<'py, f32, AllowTypeChange>,
-        a_arr: PyArrayLike1<'py, f32, AllowTypeChange>,
-    ) -> anyhow::Result<Self> {
-        if v_arr.len() != 3 {
-            bail!("Vectors array must be of size 3");
-        }
-        if a_arr.len() != 3 {
-            bail!("Angles array must be of size 3");
-        }
-        let mut v = Vector3f::zeros();
-        let mut a = Vector3f::zeros();
-        unsafe {
-            std::ptr::copy_nonoverlapping(v_arr.data(), v.as_mut_ptr(), 3);
-            std::ptr::copy_nonoverlapping(a_arr.data(), a.as_mut_ptr(), 3);
-        }
-        Ok(PeriodicBox(molar::core::PeriodicBox::from_vectors_angles(
-            v[0], v[1], v[2], a[0], a[1], a[2],
-        )?))
-    }
-
-    fn to_vectors_angles(slf: Bound<Self>) -> (Bound<PyArray1<f32>>, Bound<PyArray1<f32>>) {
-        let (v,a) = slf.borrow().0.to_vectors_angles();
-        let v_arr = copy_vec3_to_pyarray(slf.py(), &v);
-        let a_arr = copy_vec3_to_pyarray(slf.py(), &a);
-        (v_arr, a_arr)
-    }
-
-    fn shortest_vector<'py>(&self, py: Python<'py>, arr: PyArrayLike1<'py, f32, AllowTypeChange>) -> Bound<'py,PyArray1<f32>> {
-        if arr.len() != 3 {
-            panic!("Vector array must be of size 3");
-        }
-        if arr.strides()[0] != std::mem::size_of::<f32>() as isize {
-            panic!("Vector must be contigous");
-        };
-        let mut inp_v = Vector3f::zeros();
-        unsafe {
-            std::ptr::copy_nonoverlapping(arr.data(), inp_v.as_mut_ptr(), 3);
-        }
-        let out_v = self.0.shortest_vector(&inp_v);
-        copy_vec3_to_pyarray(py, &out_v)
-    }
-}
 
 #[pyclass]
 struct Topology(Option<molar::core::Topology>);
@@ -92,7 +31,7 @@ impl State {
         self.0
             .as_ref()
             .map(|s| s.len())
-            .ok_or_else(|| anyhow::anyhow!("State is moved"))
+            .ok_or_else(|| anyhow!("State is moved"))
     }
 
     #[getter]
@@ -100,7 +39,7 @@ impl State {
         self.0
             .as_ref()
             .map(|s| s.get_time())
-            .ok_or_else(|| anyhow::anyhow!("State is moved"))
+            .ok_or_else(|| anyhow!("State is moved"))
     }
 
     #[getter]
@@ -109,8 +48,8 @@ impl State {
             self.0
                 .as_ref()
                 .map(|s| s.get_box())
-                .ok_or_else(|| anyhow::anyhow!("State is moved"))?
-                .ok_or_else(|| anyhow::anyhow!("No periodic box"))?
+                .ok_or_else(|| anyhow!("State is moved"))?
+                .ok_or_else(|| anyhow!("No periodic box"))?
                 .clone(),
         ))
     }
@@ -121,8 +60,8 @@ impl State {
             .0
             .as_mut()
             .map(|s| s.get_box_mut())
-            .ok_or_else(|| anyhow::anyhow!("State is moved"))?
-            .ok_or_else(|| anyhow::anyhow!("No periodic box"))?;
+            .ok_or_else(|| anyhow!("State is moved"))?
+            .ok_or_else(|| anyhow!("No periodic box"))?;
         *b = val.borrow().0.clone();
         Ok(())
     }
@@ -160,7 +99,7 @@ impl FileHandler {
             self.0.write(&s.0)?;
         } else if let Ok(s) = data.downcast::<PyTuple>() {
             if s.len() != 2 {
-                return Err(anyhow::anyhow!("Tuple must have two elements"));
+                return Err(anyhow!("Tuple must have two elements"));
             }
             let top = s
                 .iter()
@@ -169,7 +108,7 @@ impl FileHandler {
                 .extract::<PyRefMut<'_, Topology>>()?
                 .0
                 .take()
-                .ok_or_else(|| anyhow::anyhow!("Topology is moved"))?;
+                .ok_or_else(|| anyhow!("Topology is moved"))?;
             let st = s
                 .iter()
                 .next()
@@ -177,10 +116,10 @@ impl FileHandler {
                 .extract::<PyRefMut<'_, State>>()?
                 .0
                 .take()
-                .ok_or_else(|| anyhow::anyhow!("State is moved"))?;
+                .ok_or_else(|| anyhow!("State is moved"))?;
             self.0.write(&(top, st))?;
         } else {
-            return Err(anyhow::anyhow!(
+            return Err(anyhow!(
                 "Invalid data type {} when writing to file",
                 data.get_type()
             ));
@@ -194,12 +133,10 @@ impl FileHandler {
         } else if let Ok(s) = data.extract::<PyRef<'_, Sel>>() {
             self.0.write_topology(&s.0)?;
         } else if let Ok(mut s) = data.extract::<PyRefMut<'_, Topology>>() {
-            self.0.write_topology(
-                &s.0.take()
-                    .ok_or_else(|| anyhow::anyhow!("Topology is moved"))?,
-            )?;
+            self.0
+                .write_topology(&s.0.take().ok_or_else(|| anyhow!("Topology is moved"))?)?;
         } else {
-            return Err(anyhow::anyhow!(
+            return Err(anyhow!(
                 "Invalid data type {} when writing to file",
                 data.get_type()
             ));
@@ -213,12 +150,10 @@ impl FileHandler {
         } else if let Ok(s) = data.extract::<PyRef<'_, Sel>>() {
             self.0.write_state(&s.0)?;
         } else if let Ok(mut s) = data.extract::<PyRefMut<'_, State>>() {
-            self.0.write_state(
-                &s.0.take()
-                    .ok_or_else(|| anyhow::anyhow!("State is moved"))?,
-            )?;
+            self.0
+                .write_state(&s.0.take().ok_or_else(|| anyhow!("State is moved"))?)?;
         } else {
-            return Err(anyhow::anyhow!(
+            return Err(anyhow!(
                 "Invalid data type {} when writing to file",
                 data.get_type()
             ));
@@ -254,7 +189,7 @@ impl Source {
                 topology.0.take().unwrap().into(),
                 state.0.take().unwrap().into(),
             )
-            .map_err(|e| anyhow::anyhow!(e))?,
+            .map_err(|e| anyhow!(e))?,
         ))
     }
 
@@ -265,50 +200,43 @@ impl Source {
     #[staticmethod]
     fn from_file(fname: &str) -> PyResult<Self> {
         Ok(Source(
-            molar::core::Source::builder_from_file(fname).map_err(|e| anyhow::anyhow!(e))?,
+            molar::core::Source::builder_from_file(fname).map_err(|e| anyhow!(e))?,
         ))
     }
 
     fn select_all(&mut self) -> PyResult<Sel> {
-        Ok(Sel(self.0.select_all().map_err(|e| anyhow::anyhow!(e))?))
+        Ok(Sel(self.0.select_all().map_err(|e| anyhow!(e))?))
     }
 
     fn select_str(&mut self, sel_str: &str) -> PyResult<Sel> {
-        Ok(Sel(self
-            .0
-            .select_str(sel_str)
-            .map_err(|e| anyhow::anyhow!(e))?))
+        Ok(Sel(self.0.select_str(sel_str).map_err(|e| anyhow!(e))?))
     }
 
     #[pyo3(signature = (arg=None))]
     fn __call__(&self, arg: Option<&Bound<'_, PyAny>>) -> PyResult<Sel> {
         if let Some(arg) = arg {
-            if arg.is_instance_of::<PyString>() {
+            if let Ok(val) = arg.extract::<String>() {
+                if val.is_empty() {
+                    Ok(Sel(self.0.select_all().map_err(|e| anyhow!(e))?))
+                } else {
+                    Ok(Sel(self.0.select_str(val).map_err(|e| anyhow!(e))?))
+                }
+            } else if let Ok(val) = arg.extract::<(usize, usize)>() {
                 Ok(Sel(self
                     .0
-                    .select_str(arg.extract::<String>()?)
-                    .map_err(|e| anyhow::anyhow!(e))?))
-            } else if arg.is_instance_of::<PyTuple>() {
-                let (i1, i2) = arg.extract()?;
-                Ok(Sel(self
-                    .0
-                    .select_range(i1..i2)
-                    .map_err(|e| anyhow::anyhow!(e))?))
-            } else if arg.is_instance_of::<PyList>() {
-                let ind = arg.extract()?;
-                Ok(Sel(self
-                    .0
-                    .select_vec(ind)
-                    .map_err(|e| anyhow::anyhow!(e))?))
+                    .select_range(val.0..val.1)
+                    .map_err(|e| anyhow!(e))?))
+            } else if let Ok(val) = arg.extract::<Vec<usize>>() {
+                Ok(Sel(self.0.select_vec(val).map_err(|e| anyhow!(e))?))
             } else {
-                Err(anyhow::anyhow!(
+                Err(anyhow!(
                     "Invalid argument type {} when creating selection",
                     arg.get_type()
                 )
                 .into())
             }
         } else {
-            Ok(Sel(self.0.select_all().map_err(|e| anyhow::anyhow!(e))?))
+            Ok(Sel(self.0.select_all().map_err(|e| anyhow!(e))?))
         }
     }
 }
@@ -333,25 +261,20 @@ impl Sel {
     }
 
     fn __call__(&self, arg: &Bound<'_, PyAny>) -> PyResult<Sel> {
-        if arg.is_instance_of::<PyString>() {
+        if let Ok(val) = arg.extract::<String>() {
+            Ok(Sel(self.0.subsel_str(val).map_err(|e| anyhow!(e))?))
+        } else if let Ok(val) = arg.extract::<(usize, usize)>() {
             Ok(Sel(self
                 .0
-                .subsel_str(arg.extract::<String>()?)
-                .map_err(|e| anyhow::anyhow!(e))?))
-        } else if arg.is_instance_of::<PyTuple>() {
-            let (i1, i2) = arg.extract()?;
+                .subsel_local_range(val.0..val.1)
+                .map_err(|e| anyhow!(e))?))
+        } else if let Ok(val) = arg.extract::<Vec<usize>>() {
             Ok(Sel(self
                 .0
-                .subsel_local_range(i1..i2)
-                .map_err(|e| anyhow::anyhow!(e))?))
-        } else if arg.is_instance_of::<PyList>() {
-            let ind: Vec<usize> = arg.extract()?;
-            Ok(Sel(self
-                .0
-                .subsel_iter(ind.into_iter())
-                .map_err(|e| anyhow::anyhow!(e))?))
+                .subsel_iter(val.into_iter())
+                .map_err(|e| anyhow!(e))?))
         } else {
-            Err(anyhow::anyhow!(
+            Err(anyhow!(
                 "Invalid argument type {} when creating selection",
                 arg.get_type()
             )
@@ -364,7 +287,7 @@ impl Sel {
         let s = slf.borrow();
         let ind = if i < 0 {
             if i.abs() > s.__len__() as isize {
-                return Err(anyhow::anyhow!(
+                return Err(anyhow!(
                     "Negative index {i} is out of bounds {}:-1",
                     -(s.__len__() as isize)
                 )
@@ -372,7 +295,7 @@ impl Sel {
             }
             s.__len__() - i.unsigned_abs()
         } else if i >= s.__len__() as isize {
-            return Err(anyhow::anyhow!("Index {} is out of bounds 0:{}", i, s.__len__()).into());
+            return Err(anyhow!("Index {} is out of bounds 0:{}", i, s.__len__()).into());
         } else {
             i as usize
         };
@@ -387,10 +310,10 @@ impl Sel {
         .into_py_any(slf.py())?)
     }
 
-    fn com<'a>(&self, _py: Python<'a>) -> PyResult<Bound<'a, numpy::PyArray1<f32>>> {
-        Ok(copy_vec3_to_pyarray(
-            _py,
-            &self.0.center_of_mass().map_err(|e| anyhow::anyhow!(e))?.coords,
+    fn com<'a>(&self, py: Python<'a>) -> PyResult<Bound<'a, numpy::PyArray1<f32>>> {
+        Ok(clone_vec_to_pyarray1(
+            &self.0.center_of_mass().map_err(|e| anyhow!(e))?.coords,
+            py,
         ))
     }
 
@@ -408,6 +331,9 @@ impl Sel {
     }
 
     fn get_coord<'py>(&self, py: Python<'py>) -> Bound<'py, PyAny> {
+        // We don't want to create a matrix and call to_pyarray() because it copies the data.
+        // Instead we allocate an uninitialized PyArray manually and fill it by data.
+        // By doing this we save copying potentially large array.
         use numpy::Element;
         use numpy::PyArrayDescrMethods;
 
@@ -422,14 +348,18 @@ impl Sel {
                 dims.as_dims_ptr(),
                 std::ptr::null_mut(), // no strides
                 std::ptr::null_mut(), // no data, allocate new buffer
-                npyffi::NPY_ARRAY_WRITEABLE | npyffi::NPY_ARRAY_OWNDATA, // flag
+                npyffi::NPY_ARRAY_WRITEABLE
+                    | npyffi::NPY_ARRAY_OWNDATA
+                    | npyffi::NPY_ARRAY_F_CONTIGUOUS, // flag
                 std::ptr::null_mut(), // obj
             ) as *mut npyffi::PyArrayObject;
 
-            let ptr = (*arr).data.cast::<f32>();
-            for i in 0..self.__len__() {
+            // Copy coordinates directly into uninitialized array buffer
+            let mut ptr = (*arr).data.cast::<f32>();
+            for i in 0..self.0.len() {
                 let pos = self.0.nth_pos_unchecked(i);
-                std::ptr::copy_nonoverlapping(pos.coords.as_ptr(), ptr.offset(i as isize * 3), 3);
+                std::ptr::copy_nonoverlapping(pos.coords.as_ptr(), ptr, 3);
+                ptr = ptr.add(3);
             }
 
             Bound::from_owned_ptr(py, arr.cast())
@@ -439,7 +369,7 @@ impl Sel {
     fn set_coord(&mut self, arr: PyReadonlyArray2<f32>) -> PyResult<()> {
         // Check if the shape is correct
         if arr.shape() != [3, self.__len__()] {
-            return Err(anyhow::anyhow!(
+            return Err(anyhow!(
                 "Array shape must be [3, {}], not {:?}",
                 self.__len__(),
                 arr.shape()
@@ -481,50 +411,19 @@ impl ParticleIterator {
     }
 }
 
-// Constructs PyArray backed by existing Pos data.
-fn map_pyarray_to_pos<'py>(
-    py: Python<'py>,
-    data: &mut molar::core::Pos,
-    parent: &Bound<'py, PyAny>,
-) -> *mut npyffi::PyArrayObject {
-    use numpy::Element;
-    use numpy::PyArrayDescrMethods;
-
-    let mut dims = numpy::ndarray::Dim(3);
-
-    unsafe {
-        let ptr = PY_ARRAY_API.PyArray_NewFromDescr(
-            py,
-            PY_ARRAY_API.get_type_object(py, npyffi::NpyTypes::PyArray_Type),
-            f32::get_dtype(py).into_dtype_ptr(),
-            dims.ndim_cint(),
-            dims.as_dims_ptr(),
-            std::ptr::null_mut(),                    // no strides
-            data.coords.as_mut_ptr() as *mut c_void, // data
-            npyffi::NPY_ARRAY_WRITEABLE,             // flag
-            std::ptr::null_mut(),                    // obj
-        );
-
-        // The following mangling with the ref counting is deduced by
-        // tries and errors and seems to work correctly and keeps the parnet object alive
-        // until any of the referencing PyArray objects is alive.
-
-        // We set the parent as a base object of the PyArray to link them together.
-        PY_ARRAY_API.PyArray_SetBaseObject(py, ptr.cast(), parent.as_ptr());
-        // Increase reference count of parent object since
-        // our PyArray is now referencing it!
-        pyo3::ffi::Py_IncRef(parent.as_ptr());
-        ptr.cast()
-    }
-}
-
-fn copy_vec3_to_pyarray<'py>(py: Python<'py>, data: &Vector3f) -> Bound<'py, PyArray1<f32>> {
-    unsafe {
-        let array = PyArray1::<f32>::new(py, 3, true);
-        std::ptr::copy_nonoverlapping(data.as_ptr(), array.data(), 3);
-        array
-    }
-}
+// fn cover(arr: Bound<'_,PyArray1<f32>>) -> nalgebra::VectorView<f32,Const<3>> {
+//     arr.try_as_matrix()
+//     let shape = arr.shape();
+//     let strides = arr.strides();
+//     let storage = unsafe {
+//         nalgebra::ViewStorage::from_raw_parts(
+//             arr.data(),
+//             (Const::<3>,Const::<1>),
+//             (Const::<1>,Const::<3>),
+//         )
+//     };
+//     nalgebra::VectorView::from_data(storage)
+// }
 
 //====================================
 
