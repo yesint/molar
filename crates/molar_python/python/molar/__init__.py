ab83634fab7682a122e5f5b179daec4189e3816a
yesint <yesint4@yahoo.com>
2025-01-17 20:20:06 +0100

    Added time suffixes and custom args to python trajectory reader

--- crates/molar_python/python/molar/__init__.py
+++ crates/molar_python/python/molar/__init__.py
@@ -1,28 +1,65 @@
 from .molar import *
 import argparse
 
+def _process_suffix(s):
+    if s=='':
+        return (None,None)
+    fr = None
+    t = None
+    if s[-2:].isnumeric():
+        #No suffix, frames provided
+        fr = int(s)
+    elif s[-2:] == 'ps':
+        t = int(s[:-2])
+    elif s[-2:] == 'ns':
+        t = int(s[:-2])*1000
+    elif s[-2:] == 'us':
+        t = int(s[:-2])*1000_000
+    return (fr,t)
+
+
 class AnalyisTask:
     def __init__(self):
         parser = argparse.ArgumentParser('Trajectory processor')
         parser.add_argument('-f','--files',nargs='+')
         parser.add_argument('--log',default=100,type=int)
-        parser.add_argument('-b','--begin',default=0,type=int)
-        parser.add_argument('-e','--end',default=-1,type=int)
-        args = parser.parse_args()
-        for a in args.files:
-            print(a)
+        parser.add_argument('-b','--begin',default='')
+        parser.add_argument('-e','--end',default='')
 
-        if len(args.files) < 2:
+        # Register user supplied arguments
+        self.register_args(parser)
+        # Parse arguments
+        self.args = parser.parse_args()
+        
+        if len(self.args.files) < 2:
             raise Exception('At least one trajectory file is required')
         
-        self.top = FileHandler(args.files[0]).read_topology()
+        self.top = FileHandler(self.args.files[0]).read_topology()
+
+        bfr,bt = _process_suffix(self.args.begin)
+        efr,et = _process_suffix(self.args.end)
 
         # Read trajectories and call process_frame on each frame
         self.consumed_frames = 0
-        for trj_file in args.files[1:]:
+        begin_skipped = False
+        for trj_file in self.args.files[1:]:
             print(f'Processing trajectory "{trj_file}"...')
             trj_handler = FileHandler(trj_file)
+            
+            if not begin_skipped:
+                if bfr:
+                    trj_handler.skip_to_frame(bfr)
+                elif bt:
+                    trj_handler.skip_to_time(bt)
+                begin_skipped = True
+            
             for st in trj_handler:
+                # See if end is reached
+                if efr and self.consumed_frames >= efr:
+                    break
+                if et and st.time > et:
+                    break
+
                 self.state = st
                 if self.consumed_frames == 0:
                     self.src = Source(self.top,self.state)
@@ -31,8 +68,8 @@ class AnalyisTask:
                 else:
                     self.src.set_state(self.state)
                         
-                if self.consumed_frames % args.log == 0:
-                    self.log_time()
+                if self.consumed_frames % self.args.log == 0:
+                    self.__log_time()
 
                 self.consumed_frames += 1
                 self.process_frame()
@@ -41,7 +78,7 @@ class AnalyisTask:
         self.post_process()
 
 
-    def log_time(self):
+    def __log_time(self):
         if self.state.time < 1000.0:
             t = f"{self.state.time} ps"
         elif self.state.time < 1000_000.0:
@@ -49,6 +86,10 @@ class AnalyisTask:
         else:
             t = f"{self.state.time/1000_000.0} us"
         print(f'Frame {self.consumed_frames}, time {t}')
+
+
+    def register_args(self,parser):
+        pass
 
 
     def pre_process(self):
