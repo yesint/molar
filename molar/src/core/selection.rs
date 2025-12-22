3d9bf5003a9815a755ab239018e642b29c5a5437
yesint <yesint4@yahoo.com>
2025-12-22 21:15:05 +0100

    Removed SelShared stub

--- molar/src/core/selection.rs
+++ molar/src/core/selection.rs
@@ -24,8 +24,6 @@ pub use traits::*;
 mod par_split;
 pub use par_split::*;
 
-mod sel_shared;
-
 use super::{selection_parser::SelectionParserError, BuilderError, PeriodicBoxError};
 use crate::io::FileIoError;
 use thiserror::Error;
